e1bed51f2d0adb82fc574d8a03d269d319eadf55
Pascal Fares <pascal.fares@cofares.net>
2022-04-21 18:08:15 +0300

    netoyer pom

--- Poseiden-skeleton/src/main/java/com/nnk/springboot/auth/CustomOAuth2User.java
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/auth/CustomOAuth2User.java
@@ -8,26 +8,26 @@ import org.springframework.security.oauth2.core.user.OAuth2User;
 
 public class CustomOAuth2User implements OAuth2User {
 
-	private OAuth2User oauth2User;
+    private OAuth2User oauth2User;
 
-	public CustomOAuth2User(OAuth2User user) {
-		this.oauth2User = user;
-	}
+    public CustomOAuth2User(OAuth2User user) {
+        this.oauth2User = user;
+    }
 
-	@Override
-	public Map<String, Object> getAttributes() {
-		return oauth2User.getAttributes();
-	}
+    @Override
+    public Map<String, Object> getAttributes() {
+        return oauth2User.getAttributes();
+    }
 
-	@Override
-	public Collection<? extends GrantedAuthority> getAuthorities() {
-		return oauth2User.getAuthorities();
-	}
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return oauth2User.getAuthorities();
+    }
 
-	@Override
-	public String getName() {
-		Map<String, Object> attributs = getAttributes();
-		String pseudo = (String) attributs.get("login");
-		return pseudo;
-	}
+    @Override
+    public String getName() {
+        Map<String, Object> attributs = getAttributes();
+        String pseudo = (String) attributs.get("login");
+        return pseudo;
+    }
 }
